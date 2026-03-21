860d4a3d6a05efbb0722f31002cfd26c91243e83
aprzn <novaberman@proton.me>
2026-03-21 10:59:21 -0400

    getting started on taskit fix

--- src/common.rs
+++ src/common.rs
@@ -39,149 +39,7 @@ pub enum DeltaItem {
     DeleteTag(String),
 }
 
-pub mod error {
-    use std::{error::Error, fmt::Display, io};
-
-    use inquire::InquireError;
-
-    #[derive(Debug)]
-    pub struct TaskitError {
-        pub kind: Kind,
-        pub source: Source
-    }
-
-    #[derive(Debug)]
-    pub enum Kind {
-        Cancelled,
-        CategoryArchived(String),
-        NoSuchCategory(String),
-        DuplicateCategory(String),
-        CategoryNotEmpty(String),
-        Other(Box<dyn Error>),
-        NoSpaceInTag,
-    }
-
-    /// Used in TaskitError for "error occurred while attempting to [...]"
-    #[derive(Debug)]
-    pub enum Source {
-        CreatingTag,
-        CreatingEntry,
-        CreatingCategory,
-        RunningStopwatch,
-        SelectingEntry,
-        EditingEntry,
-        ArchivingCategory,
-        UpdatingTag,
-        EditingNote,
-        UpdatingCategory,
-        DrawingTui,
-        SettingFilter,
-        ConfirmingDelete,
-        DeletingCategory,
-        DeletingTag,
-    }
-
-    pub type TaskitResult<T> = Result<T, TaskitError>;
-
-    impl Source {
-        fn activity(&self) -> &'static str {
-            match self {
-                Source::CreatingTag => "creating a tag",
-                Source::CreatingEntry => "creating an entry",
-                Source::CreatingCategory => "creating a category",
-                Source::RunningStopwatch => "stopwatch was running",
-                Source::SelectingEntry => "selecting an entry to edit",
-                Source::EditingEntry => "editing an entry",
-                Source::ArchivingCategory => "archiving a category",
-                Source::UpdatingTag => "updating a tag",
-                Source::EditingNote => "editing a daily note",
-                Source::UpdatingCategory => "updating a category",
-                Source::DrawingTui => "performing TUI operations",
-                Source::SettingFilter => "setting a filter",
-                Source::ConfirmingDelete => "confirming deletion",
-                Source::DeletingCategory => "deleting a category",
-                Source::DeletingTag => "deleting a tag",
-            }
-        }
-    }
-
-    impl Display for TaskitError {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            let Self {kind, source} = self;
-            let activity = source.activity();
-            match kind {
-                Kind::Cancelled => write!(f, "User cancelled while {activity}."),
-                Kind::CategoryArchived(c) => write!(f, "Operation ({activity}) could not be completed because category {c} is archived."),
-                Kind::NoSuchCategory(c) => write!(f, "While {activity}, tried to use category '{c}', which doesn't exist."),
-                Kind::DuplicateCategory(c) => write!(f, "While {activity}, tried to create category '{c}', which already exists."),
-                Kind::CategoryNotEmpty(c) => write!(f, "Category {c} was not empty while {activity}."),
-                Kind::NoSpaceInTag => write!(f, "Spaces aren't allowed in tags. Occurred while {activity}."),
-                Kind::Other(error) => write!(f, "An external error occurred while {activity}: {error}"),
-            }
-        }
-    }
-
-    impl Error for TaskitError { }
-
-    impl From<(InquireError, Source)> for TaskitError {
-        fn from((value, source): (InquireError, Source)) -> Self {
-            match value {
-                InquireError::NotTTY => panic!("Taskit assumes it is running in an interactive terminal"),
-                InquireError::InvalidConfiguration(s) => panic!("internal error: invalid configuration\n{}", s),
-                InquireError::IO(error) => error.with(source).into(),
-                InquireError::OperationCanceled => Self { kind: Kind::Cancelled, source },
-                InquireError::OperationInterrupted => Self{ kind: Kind::Cancelled, source },
-                InquireError::Custom(error) => Self { kind: Kind::Other(error), source },
-            }
-        }
-    }
-
-    impl From<(io::Error, Source)> for TaskitError {
-        fn from((value, source): (io::Error, Source)) -> Self {
-            Self { kind: Kind::Other(Box::new(value)), source }
-        }
-    }
-
-    /// attaches source data to a given error
-    pub trait With<T>: Sized {
-        type Joined;
-        fn with(self, source: T) -> Self::Joined;
-    }
-
-    impl With<Source> for io::Error {
-        type Joined = (Self, Source);
-
-        fn with(self, source: Source) -> Self::Joined {
-            (self, source)
-        }
-    }
-
-    impl With<Source> for InquireError {
-        type Joined = (Self, Source);
-
-        fn with(self, source: Source) -> Self::Joined {
-            (self, source)
-        }
-    }
-
-    impl With<Source> for Kind {
-        type Joined = TaskitError;
-
-        fn with(self, source: Source) -> Self::Joined {
-            TaskitError { kind: self, source }
-        }
-    }
-
-    impl<T, E> With<Source> for Result<T, E>
-    where E: With<Source>
-    {
-        type Joined = Result<T, E::Joined>;
-
-        fn with(self, source: Source) -> Self::Joined {
-            self.map_err(|e| e.with(source))
-        }
-    }
-}
+pub mod error;
 
 #[derive(Clone)]
 pub struct TagCompleter<'a>(pub &'a [String]);
@@ -438,6 +296,38 @@ impl Sub for SimpleTime {
     }
 }
 
+pub struct InvariantsChecker {
+    save_data: SaveData
+}
+
+enum BrokenInvariant {
+    /// bool: true if in archived_categories
+    CategoryNotUnique(Vec<(String, bool)>), // manual
+    TagNotUnique(String), // automatic
+    TagContainsSpace(String), // sometimes automatic
+    NonCategoryTagMapKey(String), // automatic
+    NonTagTagMapValue(String), // automatic
+    NonexistentEventCategory(Event, usize), // manual
+    NonexistentEventTag(Event, usize), // manual
+    EventTagDescMismatch(Event, usize), // automatic
+}
+
+impl InvariantsChecker {
+    pub fn new(save_data: SaveData) -> Self {
+        Self {
+            save_data
+        }
+    }
+}
+
+impl Iterator for InvariantsChecker {
+    type Item = BrokenInvariant;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+}
+
 trait Upgrade {
     type Next;
     fn upgrade(self) -> Self::Next;
@@ -529,6 +419,16 @@ impl Default for SaveDataVersioned {
     }
 }
 
+/// Invariants:
+/// - each element of `categories` U `archived_categories` must be unique
+/// - each element of `tags` should be unique
+/// - no element of `tags` should contain a space
+/// - every key in tag_map should be an element of `categories`
+/// - every element of every value of tag_map should be an element of `tags`
+/// - for each element of `events`:
+///   - `event.category` should be an element of `categories` U `archived_categories`
+///   - each element of `event.tags` should be an element of `tags`
+///   - `event.tags` should equal the list of words prefixed with `#` in event.description
 pub type SaveData = SaveDataV6;
 
 impl SaveDataVersioned {
